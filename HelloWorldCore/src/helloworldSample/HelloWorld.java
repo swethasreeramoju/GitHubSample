db95b14a31ba8d533a272ffa6dbfa0f6e6019400
swetha_sreeramoju <swetha_sreeramoju@DLF-8F-D170.semanticindia.com>
2015-04-16 14:33:55 +0530

    new commit

--- HelloWorldCore/src/helloworldSample/HelloWorld.java
+++ HelloWorldCore/src/helloworldSample/HelloWorld.java
@@ -2,6 +2,6 @@ package helloworldSample;
 
 public class HelloWorld {
 	public static void main(String args[]){
-		System.out.println("Hello World.12345.");
+		System.out.println("Hello World.12345 .");
 	}
 }
